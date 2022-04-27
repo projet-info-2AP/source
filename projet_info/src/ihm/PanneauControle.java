81e9a0a9b8c05644385a4e070adc669002a9d6ec
marieinthehouse <104509734+marieinthehouse@users.noreply.github.com>
2022-04-27 16:40:45 +0200

    Update PanneauControle.java

--- projet_info/src/ihm/PanneauControle.java
+++ projet_info/src/ihm/PanneauControle.java
@@ -16,14 +16,14 @@ public class PanneauControle extends JPanel{
     //checkboxe affichage lignes de champs 
 
 
-LigneDeChampButton = new JCheckBox(« Ligne de Champ »);
+LigneDeChampButton = new JCheckBox(" Ligne de Champ ");
    LigneDeChampButton.setMnemonic(KeyEvent.VK_C); 
     LigneDeChampButton.setSelected(true);
 
 
 // checkboxe affichage flèches de lignes de champ
 
-FlecheLigneDeChampButton = new JCheckBox(« Flèches de ligne de Champ »);
+FlecheLigneDeChampButton = new JCheckBox(" Flèches de ligne de Champ ");
    FlecheLigneDeChampButton.setMnemonic(KeyEvent.VK_C); 
     FlecheLigneDeChampButton.setSelected(true);
 
@@ -31,13 +31,13 @@ FlecheLigneDeChampButton = new JCheckBox(« Flèches de ligne de Champ »);
 //checkbox affichage équipotentielle
 
 
-EquipotentielleButton = new JCheckBox(« Equipotentielle »);
+EquipotentielleButton = new JCheckBox("Equipotentielle");
    EquipotentielleButton.setMnemonic(KeyEvent.VK_C); 
     EquipotentielleButton.setSelected(true);
     
 // checkbox affichage gradien de couleur
 
-ColorButton = new JCheckBox(« Gradien de couleurs »);
+ColorButton = new JCheckBox("Gradien de couleurs ");
    ColorButton.setMnemonic(KeyEvent.VK_C); 
     ColorButton.setSelected(true);
 
@@ -67,5 +67,4 @@ public void itemStateChanged(ItemEvent e) {
     ...
     updatePanneauprincipal();
 }
-
 }
